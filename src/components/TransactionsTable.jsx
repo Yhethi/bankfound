1b1d26f13a6bbe589f44857e178cc08e56b3e000
Yhethi <eloyjosebonillag@gmail.com>
2025-01-05 21:59:51 -0400

    Table style min-width scroll

--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -9,28 +9,30 @@ const TransactionsTable = () => {
     },
   }));
   return (
-    <table className="transactions-table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Saldo</th>
-          <th>Ahorrado</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, index) => (
-          <tr key={index}>
-            <td>{user.user.id}</td>
-            <td>{user.user.name}</td>
-            <td>{user.user.email}</td>
-            <td>${user.user.saldo}</td>
-            <td>${user.user.ahorrado}</td>
+    <div class="table-container">
+      <table className="transactions-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Saldo</th>
+            <th>Ahorrado</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.user.id}</td>
+              <td>{user.user.name}</td>
+              <td>{user.user.email}</td>
+              <td>${user.user.saldo}</td>
+              <td>${user.user.ahorrado}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
