bcc4b9a3574596ed990ad86d7b9fe9204b6448ab
Yhethi <eloyjosebonillag@gmail.com>
2025-01-11 18:17:03 -0400

    v1.03

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="footer__container">
-      Hecho por Yhethi +584124706698 <p className="v_foot zIndexUp">v1.00</p>
+      Hecho por Yhethi +584124706698 <p className="v_foot zIndexUp">v1.03</p>
     </div>
   );
 };
