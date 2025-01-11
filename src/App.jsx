04469ba192078f9149ad565d3382034cdc3af14d
Yhethi <eloyjosebonillag@gmail.com>
2025-01-10 23:35:47 -0400

    Build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import "./styles/main.scss";
 import Banking from "./pages/Banking";
-import Sidebar from "./components/Sidebar";
 import { BackgroundAnimated } from "./components/BackgroundAnimated";
 import Loader from "./components/tools/Loader";
-import { useSelector } from "react-redux";
 
 function App() {
   return (
